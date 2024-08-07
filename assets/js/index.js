a5750429fb97f4c4a31b19b0c6b62a01bf522733
Oscar <oscarwebster96@gmail.com>
2024-08-07 19:03:22 -0400

    se termina contacto y se sacan detalles de navbar y card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,7 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+$(document).ready(function() {
+    $(".hola").click(function() {
+        alert("Envío exitoso");
+    });
+});
