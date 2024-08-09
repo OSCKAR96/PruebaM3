1bb3c4f591c7abb9f4ff0757db67beef365204ee
Oscar <oscarwebster96@gmail.com>
2024-08-09 17:31:19 -0400

    termine la prueba porfin

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 $(document).ready(function() {
     $(".hola").click(function() {
         alert("Envío exitoso");
